47a5a108429fc1da53e9dcd7b49dbb94fa5b90d4
Andrew Jones <ascjones@gmail.com>
2016-02-09 23:45:11 +0000

    Script to generate london.html from index-template

--- generate.sh
+++ generate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# awk 'BEGIN{getline l < "vars/london/banner-date"}/{{ banner-date }}/{gsub("{{ banner-date }}",l)}1' index-template.html > london.html
+rm -f london.html
+cp index-template.html london.html
+
 for file in vars/london/*
 do
   name=${file##*/}
-  # awk -v var_file=$file var_name=$name 'BEGIN{getline l < var_file}/{{ var_name }}/{gsub("{{ var_name }}",l)}1' index-template.html
-  awk -v var_file=$file -v var_name=$name 'BEGIN{getline l < var_file}/{{ banner-date }}/{gsub("{{ var_name }}",l)}1' index-template.html
-  # awk -v name=$file -v age=$name 'BEGIN{print name, age}'
-done # > london.html
+  sed -i -e "/{{ $name }}/ r $file" -e "s/{{ $name }}//" london.html
+done 
