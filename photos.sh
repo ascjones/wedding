e8a7c827d36148f7d9be16892a09027e9d1f103d
Andrew Jones <ascjones@gmail.com>
2016-07-23 15:30:22 +0100

    Replace main-img strings in template

--- photos.sh
+++ photos.sh
@@ -9,23 +9,16 @@ case "$1" in
     mkdir -p ${THUMBS_DIR}
     mkdir -p ${MEDIUM_DIR}
 
-    for file in ${PHOTOS_DIR}/*
+    for file in ${PHOTOS_DIR}/*.jpg
     do
-      # next line checks the mime-type of the file
-      IMAGE_TYPE=`file --mime-type -b "$file" | awk -F'/' '{print $1}'`
-      if [ x$IMAGE_TYPE = "ximage" ]; then
-          WIDTH=`identify -format "%w" "$file"`
-          HEIGHT=`identify -format "%h" "$file"`
-          # If the image width is greater that 200 or the height is greater that 150 a thumb is created
-         if [ $WIDTH -ge  201 ] || [ $HEIGHT -ge 151 ]; then
-            filename=$(basename "$file")
-            extension="${filename##*.}"
-            filename="${filename%.*}"
+      WIDTH=`identify -format "%w" "$file"`
+      HEIGHT=`identify -format "%h" "$file"`
+      filename=$(basename "$file")
+      extension="${filename##*.}"
+      filename="${filename%.*}"
 
-            convert -sample 200x150 -strip "$file" "${THUMBS_DIR}//${filename}_thumb.png"
-            convert -resize 50% "$file" "${MEDIUM_DIR}//${filename}_medium.${extension}"
-         fi
-      fi
+      convert -sample 200x150 -strip "$file" "${THUMBS_DIR}//${filename}_thumb.png"
+      convert -resize 50% "$file" "${MEDIUM_DIR}//${filename}_medium.${extension}"
     done
     ;;
   html)
@@ -34,9 +27,16 @@ case "$1" in
       WIDTH=`identify -format "%w" "$file"`
       HEIGHT=`identify -format "%h" "$file"`
       name=${file##*/}
-      thumb_img=$THUMBS_DIR/
 
-      sed -e "s/{{ main-img }}/$name/g" photo-template.html
+      filename=$(basename "$file")
+      extension="${filename##*.}"
+      filename="${filename%.*}"
+
+      thumb_img=${THUMBS_DIR}/${filename}_thumb.png
+
+      sed \
+        -e "s/{{ main-img }}/photos\/london\/$name/g" \
+        -e "s/{{ main-img-size }}/${WIDTH}x${HEIGHT}/g" photo-template.html
     done
     ;;
 esac
