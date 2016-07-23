2362fdb5a53c4625b5b07b7c6e96cb6bd4e40020
Andrew Jones <ascjones@gmail.com>
2016-07-23 22:53:58 +0100

    Replace remaining vars in html template

--- photos.sh
+++ photos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PHOTOS_DIR=photos/london
+AUTHOR=todo
 THUMBS_DIR=${PHOTOS_DIR}/thumbs
 MEDIUM_DIR=${PHOTOS_DIR}/medium
 
@@ -24,19 +25,24 @@ case "$1" in
   html)
     for file in ${PHOTOS_DIR}/*.jpg
     do
-      WIDTH=`identify -format "%w" "$file"`
-      HEIGHT=`identify -format "%h" "$file"`
       name=${file##*/}
 
       filename=$(basename "$file")
-      extension="${filename##*.}"
       filename="${filename%.*}"
 
       thumb_img=${THUMBS_DIR}/${filename}_thumb.png
+      med_img=${MEDIUM_DIR}/${filename}_medium.jpg
+
+      main_img_dim=`identify -format "%wx%h" "$file"`
+      med_img_dim=`identify -format "%wx%h" "$med_img"`
 
       sed \
-        -e "s/{{ main-img }}/photos\/london\/$name/g" \
-        -e "s/{{ main-img-size }}/${WIDTH}x${HEIGHT}/g" photo-template.html
+        -e "s~{{ main-img }}~photos/london/$name~g" \
+        -e "s~{{ main-img-size }}~$main_img_dim~g" photo-template.html \
+        -e "s~{{ med-img }}~$med_img~g" photo-template.html \
+        -e "s~{{ med-img-size }}~$med_img_dim~g" photo-template.html \
+        -e "s~{{ img-author }}~$AUTHOR~g" photo-template.html \
+        -e "s~{{ thumb-img }}~$thumb_img~g" photo-template.html
     done
     ;;
 esac
