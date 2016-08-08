9fccdc116f155edde0d55baa88a47dfc75838c29
Andrew Jones <ascjones@gmail.com>
2016-08-08 21:04:13 +0100

    Change location of generated invite pages

--- generate.sh
+++ generate.sh
@@ -5,14 +5,15 @@ echo CURRENT DIR: $(pwd)
 generate () {
   echo GENERATING $1
   town=$1
-  rm -f $town.html
-  cp index-template.html $town.html
+  dest=$town/index.html
+  rm -f $dest
+  cp index-template.html $dest
 
   for file in ./vars/$town/*
   do
     name=${file##*/}
-    sed -i.bak -e "/{{ $name }}/ r $file" -e "s/{{ $name }}//" $town.html
-    rm $town.html.bak
+    sed -i.bak -e "/{{ $name }}/ r $file" -e "s/{{ $name }}//" $dest
+    rm $dest.bak
   done
 }
 
